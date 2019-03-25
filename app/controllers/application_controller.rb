679ede301c77f53d403d8a7feead322850a21a11
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-25 17:48:52 -0400

    Add edit funtionality, update navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :current_user_object
 
   def current_user
     user_id = session["user_id"]
     @current_user ||= User.find_by(id: user_id)
     return @current_user ? @current_user.username : nil
+  end
+
+  def current_user_object
+    user_id = session["user_id"]
+    @current_user ||= User.find_by(id: user_id)
+    return @current_user ? @current_user : nil
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
