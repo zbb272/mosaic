6fe1e908f2a2237199bb5612406071db806857b3
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-26 09:39:52 -0400

    Begin writing tests, currently commented out

---
+++ test/spec/features/posts_spec.rb
@@ -0,0 +1,31 @@
+# require "rails_helper"
+# describe "Posts", type: "feature" do
+#   describe "new" do
+#     before :each do
+#       #template below
+
+
+#       # before :each do
+#   #     @cool = Category.create(name: 'Cool')
+#   #     visit 'posts/new'
+#   #     fill_in('post_title', :with => 'Feeling Awesome')
+#   #     fill_in('post_content', :with => 'This is going to be an awesome trip')
+#   #   end
+#   #
+#   #   it 'can create a post without a new category' do
+#   #     click_button('Create Post')
+#   #     expect(page).to have_content('Feeling Awesome')
+#   #   end
+#   #
+#   #   it 'can create a post with a previously created category' do
+#   #     check ('Cool')
+#   #     click_button('Create Post')
+#   #     expect(page).to have_content('Cool')
+#   #   end
+#   #
+#   #   it 'can create a post with a brand new ctegory' do
+#   #     fill_in('post_categories_attributes_0_name', with => 'Funny')
+#   #     click_button('Create Post')
+#   #     expect(page).to have_content('Funny')
+#   #   end
+#   # end
---
+++ test/spec/rails_helper.rb
@@ -0,0 +1,15 @@
+# Rspec.configure do |config|
+#   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+#   config.color = true
+#
+#   config.use_transactional_fixtures = true
+#
+#   config.infer_spec_type_from_file_location!
+#
+#   DatabaseCleaner.strategy = :truncation
+#
+#   config.after(:all) do
+#     DatabaseCleaner.clean
+#   end
+#
+# end
